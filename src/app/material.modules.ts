fb1e67ea9504fa0e3a0542d77f7d6ca3f914c83b
Steinert999 <steinert999@outlook.com>
2021-07-07 18:08:49 -0300

    add material modules

--- src/app/material.modules.ts
+++ src/app/material.modules.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 // vamos construir nossos arrays de registro
 @NgModule({
  imports: [
  MatTableModule,
- MatToolbarModule
+ MatToolbarModule,
+ MatExpansionModule,
+ MatCheckboxModule,
+ MatButtonModule,
+ MatIconModule
  ],
  exports: [
  MatTableModule,
-
+ MatToolbarModule,
+ MatExpansionModule,
+ MatCheckboxModule,
+ MatButtonModule,
+ MatIconModule
  ]
 })
 export class TodoMaterialModule{}
