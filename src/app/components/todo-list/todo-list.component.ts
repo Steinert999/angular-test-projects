d8721a05c7cd88e5d386a663d8e02c9a53d5a583
Steinert999 <steinert999@outlook.com>
2021-07-08 11:46:39 -0300

    add css changes

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,12 +1,8 @@
-import { Router } from '@angular/router';
-import { Component, OnInit, } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TodoService } from './../../services/todo.service';
 import { Todo } from 'src/app/models/todo';
-
 import {
   DialogAction,
   ConfirmDialogComponent,
@@ -14,11 +10,9 @@ import {
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-
-
   todolist$!: Observable<Todo[]>;
   constructor(private todoService: TodoService, private dialog: MatDialog) {}
 
